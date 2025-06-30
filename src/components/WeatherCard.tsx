e18a293c602b6efc705c1dd2ab1a15f5212814e7
Юлиана Анисифорова <yuliananisiforova@Mac.lan>
2025-06-30 16:23:58 +0100

    created store and slice

---
+++ src/components/WeatherCard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+const WeatherCard: FC = () => {
+    return (
+        <div>
+            WeatherCard
+        </div>
+    )
+}
+
+export default WeatherCard
---
+++ src/store/weatherSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+export const weatherSlice = createSlice({
+  name: 'weather',
+  initialState,
+  reducers: {
+
+  },
+})
+
+export const {} = weatherSlice.actions
+export default weatherSlice.reducer
